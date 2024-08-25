45262de9308b977270f2696b15a56de21549bd9e
Alexis Drai <alexis@alexisdrai.com>
2024-08-25 20:37:11 +0200

    Reformat code

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,19 @@
 'use strict';
 
-module.exports = function (app) {
+module.exports = function(app) {
+// TODO 2: Complete the necessary routes
+  app.route('/api/issues/:project').get(function(req, res) {
+    let project = req.params.project;
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+  }).post(function(req, res) {
+    let project = req.params.project;
+
+  }).put(function(req, res) {
+    let project = req.params.project;
+
+  }).delete(function(req, res) {
+    let project = req.params.project;
+
+  });
+
 };
